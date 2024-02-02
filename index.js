9141a436ba60fa0485fd54f2236a6177c9e41a42
Logan Masniuk <logan.masniuk@gmail.com>
2024-02-02 10:46:09 -0800

    Implement upload endpoint. Files can be sent via post request and stored in uploads folder

--- index.js
+++ index.js
@@ -1,15 +1,29 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
- 
-app.get('/', (req,res) => {
+
+const multer = require('multer')
+
+const storage = multer.diskStorage({
+    destination: './uploads',
+    filename: function (req, file, cb) {
+        const uniqueSuffix = Date.now();
+        cb(null,uniqueSuffix + '-' + file.originalname)
+    }
+})
+
+const upload = multer({storage: storage})
+
+app.get('/', (req, res) => {
     res.send('Yo dawg');
 })
 
-app.get('/upload', (req,res) => {
+app.post('/upload', upload.single('imageFile'), (req, res, next) => {
+    console.log(req.body);
+    console.log(req.file);
     res.send('Uploaded!');
 })
- 
-app.listen(port, ()=> {
+
+app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
 })
